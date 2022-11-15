8cec4c86fed3aa3387727147759d7e514929e03b
GiulioGilardo <116483336+GiulioGilardo@users.noreply.github.com>
2022-11-15 09:24:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   float a,b,x,y,z;
+    cin>>a>>b>>x>>y;
+    if (x<0 and y>0){
+        z=a*x-b*y;
+        cout<<z<<endl;
+    } else if (x>=0 and y<=0){
+        z=a*x*x-b*y;
+        cout<<z<<endl;
+    } else {
+        z=a*b+b*y*y;
+        cout<<z<<endl;
+    }
    return 0;
 }
